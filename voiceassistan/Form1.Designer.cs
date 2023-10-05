b6e2bf54e57d0946b08f12d18fa2f336f486113f
vboxuser <vboxuser@WINDOWS-11>
2023-10-05 16:32:06 +0300

    Add project files.

---
+++ voiceassistan/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace voiceassistan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.m1 = new System.Windows.Forms.PictureBox();
+            this.m2 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.speakToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.m1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.m2)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // m1
+            // 
+            this.m1.Image = ((System.Drawing.Image)(resources.GetObject("m1.Image")));
+            this.m1.Location = new System.Drawing.Point(0, 27);
+            this.m1.Name = "m1";
+            this.m1.Size = new System.Drawing.Size(247, 274);
+            this.m1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.m1.TabIndex = 0;
+            this.m1.TabStop = false;
+            this.m1.Click += new System.EventHandler(this.m1_Click);
+            // 
+            // m2
+            // 
+            this.m2.Image = ((System.Drawing.Image)(resources.GetObject("m2.Image")));
+            this.m2.Location = new System.Drawing.Point(0, 27);
+            this.m2.Name = "m2";
+            this.m2.Size = new System.Drawing.Size(247, 274);
+            this.m2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.m2.TabIndex = 1;
+            this.m2.TabStop = false;
+            this.m2.Click += new System.EventHandler(this.m2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.speakToolStripMenuItem,
+            this.optionsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(247, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // speakToolStripMenuItem
+            // 
+            this.speakToolStripMenuItem.Name = "speakToolStripMenuItem";
+            this.speakToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.speakToolStripMenuItem.Text = "Speak";
+            this.speakToolStripMenuItem.Click += new System.EventHandler(this.speakToolStripMenuItem_Click);
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.optionsToolStripMenuItem.Text = "Options";
+            this.optionsToolStripMenuItem.Click += new System.EventHandler(this.optionsToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(93, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Options";
+            this.label1.Visible = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 27);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(247, 277);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(247, 301);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.m1);
+            this.Controls.Add(this.m2);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Voice Assistant";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.m1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.m2)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox m1;
+        private System.Windows.Forms.PictureBox m2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem speakToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ voiceassistan/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+
+namespace voiceassistan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SpeechRecognitionEngine rengine = new SpeechRecognitionEngine();
+        SpeechSynthesizer synth =new SpeechSynthesizer();
+
+        private void m1_Click(object sender, EventArgs e)
+        {
+            m1.Visible = false;
+            rengine.RecognizeAsync();
+        }
+
+        private void m2_Click(object sender, EventArgs e)
+        {
+            m1.Visible = true;
+        }
+
+        void repeat()
+        {
+            string[] commands = { "Hi", "How are you", "Open visual studio", "Open chrome","Show commands" };
+            Choices options = new Choices(commands);
+            Grammar grammar = new Grammar(new GrammarBuilder(options));
+            rengine.LoadGrammar(grammar);
+            rengine.SetInputToDefaultAudioDevice();
+            rengine.SpeechRecognized += recognize;
+            foreach( var x in commands )
+            {
+                listBox1.Items.Add( x );
+            }
+        }
+
+        private void recognize(object sender, SpeechRecognizedEventArgs e)
+        {
+            m1.Visible = true;
+            if (e.Result.Text == "Hi")
+            {
+                synth.SpeakAsync("Hello what's up");
+            }
+            if (e.Result.Text== "Open visual studio")
+            {
+                synth.SpeakAsync("Okey I open visual studio");
+                System.Diagnostics.Process.Start("\"C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\Common7\\IDE\\devenv.exe\"");
+            }
+            if (e.Result.Text == "Open chrome")
+            {
+                synth.SpeakAsync("Okey I open chrome");
+                System.Diagnostics.Process.Start("\"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe\"");
+            }
+            if (e.Result.Text == "Show commands")
+            {
+                timer1.Start();
+                synth.SpeakAsync("Okey");
+                listBox1.Visible=true;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            repeat();
+        }
+
+        private void optionsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            m1.Visible = false;
+            m2.Visible = false;
+            label1.Visible = true;
+        }
+
+        private void speakToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            m1.Visible = true;
+            m2.Visible = true;
+            label1.Visible = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            listBox1.Visible= false;
+            synth.SpeakAsync("Closed");
+            timer1.Stop();
+        }
+    }
+}
